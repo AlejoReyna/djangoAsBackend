94c382c025665a8a5bac2e0a2d421ae7f5f897dd
Alberto Reyna <alexisreyna@MacBook-Pro-de-Alberto.local>
2023-12-19 21:17:42 -0600

    First commit

---
+++ learning_log/settings.py
@@ -0,0 +1,8 @@
+INSTALLED_APPS = [
+	'django.contrib.admin',
+	'django.contirb.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'djando.contrib.staticfiles',
+]
